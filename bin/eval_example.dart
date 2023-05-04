e8584571293b3649b899f8350e0262a4f3806344
Даниил <smit.cat72@ya.ru>
2023-05-05 00:36:37 +0500

    Добавил комментов

--- bin/eval_example.dart
+++ bin/eval_example.dart
@@ -1,14 +1,24 @@
 import 'dart:isolate';
 
 void main(List<String> arguments) async {
+  /// Делаем ссылку на dart файл
   var uri = Uri.parse(
       'https://raw.githubusercontent.com/sorgfal/eval_example/main/lib/evaled_code.dart');
+
+  /// Создаем порт для общения с изолятом
   ReceivePort port = ReceivePort();
+
+  /// Спауним изолят из ссылки(скачается само)
   await Isolate.spawnUri(uri, [], port.sendPort);
 
+  /// подписываемся на события recieve порта
   await for (var i in port) {
     print(i);
   }
 
+  /// Просто чет захотелось (почти синдром Туретта)
   await Future.delayed(Duration(seconds: 10));
 }
+
+
+/// Как мы видим код из файла lib/evaled_code.dart не импортируется внутри проекта
